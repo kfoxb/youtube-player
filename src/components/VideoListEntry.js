10ed88cf4b8bac126b067ff6e5a5b8501219277c
Kyle Bradford <kfoxb@kfoxb.com>
2017-06-30 14:02:59 -0600

    add basic component for VideoListEntry

---
+++ src/components/VideoListEntry.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class VideoListEntry extends Component {
+  render () {
+    return (
+      <h3>VideoListEntry Component</h3>
+    )
+  }
+}
+
+export default VideoListEntry
