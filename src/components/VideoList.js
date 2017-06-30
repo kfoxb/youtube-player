770f28ef71fe231416d9d6e7779ad0d0b795bf87
Kyle Bradford <kfoxb@kfoxb.com>
2017-06-30 14:01:46 -0600

    set up basic component for VideoList

---
+++ src/components/VideoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class VideoList extends Component {
+  render () {
+    return (
+      <h3>VideoList Component</h3>
+    )
+  }
+}
+
+export default VideoList
